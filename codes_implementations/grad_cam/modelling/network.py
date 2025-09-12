8bd67d470c0e93a4cde84399e02f7cce705b350e
Kushal <vankadara2004@gmail.com>
2025-09-12 19:51:13 +0530

    Grad-CAM

--- codes_implementations/grad_cam/modelling/network.py
+++ codes_implementations/grad_cam/modelling/network.py
@@ -37,6 +37,42 @@ class CNN(nn.Module):
         x = x.view(x.size(0), -1)  # Flatten the tensor for fully connected layers
         x = self.fc_layers(x)
         return x
+    
+# Dummy class for visualization purpose
+class CNNDummy(nn.Module):
+    """Convolutional Neural Network for image classification."""
+    
+    def __init__(self, num_classes=4, input_channels=3, conv_filters=[16, 32, 64], kernel_size=3, dropout_rate=0,input_dim=256):
+        super(CNNDummy, self).__init__() 
+
+        # Dynamic Convolutional layers
+        layers = []
+        in_channels = input_channels
+
+        for out_channels in conv_filters:
+            layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=1))
+            layers.append(nn.ReLU())
+            layers.append(nn.MaxPool2d(2))
+            in_channels = out_channels
+
+        self.conv_layers = nn.Sequential(*layers)
+
+        # Compute the size after convolutional layers for the fully connected layers
+        self.flattened_size = conv_filters[-1] * input_dim//(2**len(conv_filters)) * input_dim//(2**len(conv_filters))  
+
+        # Fully connected layers
+        self.fc_layers = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(1617920, 500),
+            nn.ReLU(),
+            nn.Linear(500, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.size(0), -1)  # Flatten the tensor for fully connected layers
+        x = self.fc_layers(x)
+        return x
 
 class CNNWithGAP(nn.Module):
     """Convolutional Neural Network with Global Average Pooling for image classification."""
